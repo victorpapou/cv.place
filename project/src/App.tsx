b7eb1edd2dc4641719d6393c85b738c15f365c78
tr1ckish <tr1cky.wizard890@gmail.com>
2025-01-26 15:47:41 +0300

    ok tailwind is working now finaly

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react';
 import './assets/logo.png';
 import './App.css';
 
 function App() {
-   const [count, setCount] = useState(0);
-
    return (
       <>
-         <h1>Vite + React</h1>
-         <div className='card'>
-            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+         <h1>Резюме 📄, которое работает, пока ты отдыхаешь</h1>
+         <p>Создание меньше 7 минут, одно резюме бесплатно</p>
+         <div>
+            <button className='bg-green-500'>я кнопка</button>
             <p>
                Edit <code>src/App.tsx</code> and save to test HMR
             </p>
          </div>
-         <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
       </>
    );
 }
